2c985881e3d876751f1af3edf101580333a12fe3
CL02089 <csaba.lukacs@gmail.com>
2023-10-11 20:46:31 +0200

    Solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let argArr = [...args];
 
+  console.log(argArr);
+
+  const filtered = arr.filter((element) => !argArr.includes(element));
+
+  return filtered;
 };
 
 // Do not edit below this line
